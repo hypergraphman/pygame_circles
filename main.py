00e13f71ec5af51b4decb95c52fb0c353f692ff5
hypergraphman <r_aitov@mail.ru>
2019-10-06 21:22:39 +0300

    Изменил курсор

--- main.py
+++ main.py
@@ -6,8 +6,10 @@ main_screen = pygame.display.set_mode(size)
 circles_screen = pygame.Surface(main_screen.get_size())
 background = pygame.Surface(main_screen.get_size())
 clock = pygame.time.Clock()
+pygame.mouse.set_visible(False)
 
 running = True
+mouse_pos = (0, 0)
 x_pos = 0
 y_pos = 400
 v = 100  # пикселей в секунду
@@ -25,10 +27,17 @@ def draw_stars(scr, stars, height, fps):
             star[1] = - 6
             star[3] = random.randint(32, 130)
         star[1] += int(star[3] / (fps / 4))
-        pygame.draw.rect(scr, star[2], (star[0], star[1], 2, 6), 0)
+        pygame.draw.rect(scr, star[2], (star[0], star[1], 2, 6))
+
+
+def draw_cross(scr, pos, radius):
+    pygame.draw.circle(scr, (255, 0, 0), pos, radius, 1)
+    pygame.draw.line(scr, (255, 0, 0), (pos[0] - radius - 2, pos[1]), (pos[0] + radius + 2, pos[1]), 1)
+    pygame.draw.line(scr, (255, 0, 0), (pos[0], pos[1] - radius - 2), (pos[0], pos[1] + radius + 2), 1)
 
 
 while running:
+    main_screen.fill((0, 0, 0))
     # внутри игрового цикл еще один цикл
     # приема и обработки сообщений
     for event in pygame.event.get():
@@ -41,9 +50,10 @@ while running:
             g = random.randint(0, 255)
             b = random.randint(0, 255)
             circles.append([x, y, (r, g, b)])
+        if event.type == pygame.MOUSEMOTION:
+            mouse_pos = event.pos
 
     # отрисовка и изменение свойств объектов
-    main_screen.fill((0, 0, 0))
     draw_stars(background, stars, height, fps)
     circles_screen.fill((0, 0, 0))
     circles_screen.blit(background, (0, 0))
@@ -53,6 +63,9 @@ while running:
             circle[1] += int(v / fps)
         pygame.draw.circle(circles_screen, circle[2], (circle[0], circle[1]), circle_radius)
     main_screen.blit(circles_screen, (0, 0))
+
+    draw_cross(main_screen, mouse_pos, circle_radius)
+    # pygame.draw.circle(main_screen, (255, 0, 0), mouse_pos, 2)
 
     clock.tick(fps)
 
